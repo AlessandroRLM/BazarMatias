33f34b967295fb637628dc32465e149c1abcffdd
Max <maximiliano.soto@cloud.uautonoma.cl>
2025-04-04 18:46:15 -0300

    Se movió de lugar la carpeta del modulo users de donde estaba anidada

--- backend/users/apps.py
+++ backend/users/apps.py
@@ -2,4 +2,4 @@ from django.apps import AppConfig
 
 class UsersConfig(AppConfig):
     default_auto_field = 'django_mongodb_backend.fields.ObjectIdAutoField'
-    name = 'backend.users'
+    name = 'users'

d85881f55a6c6ca34b816b132051be4befb97e2a
Max <179786503+Max180sx@users.noreply.github.com>
2025-05-25 19:15:49 -0400

    fix: test.py api reports

--- backend/reports/tests.py
+++ backend/reports/tests.py
@@ -1,6 +1,6 @@
 from django.test import TestCase
 from rest_framework.test import APIClient
-from django.contrib.auth.models import User
+from users.models import User
 from rest_framework import status
 
 class ReportPDFViewTests(TestCase):
